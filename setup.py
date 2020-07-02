16938a6f798ab1cfd85fcc056387ed781890e9ba
imjoseangel <josean.munoz@gruposantander.com>
2020-07-02 16:12:32 +0200

    Adds setup

--- setup.py
+++ setup.py
@@ -1,2 +1,19 @@
-from setuptools import setup
-setup()
+import setuptools
+
+setuptools.setup(
+    name="imjoseangel",  # Replace with your own username
+    version="1.0.0",
+    author="Jose Angel Munoz",
+    author_email="josea.munoz@gmail.com",
+    description="pre-commit-hooks",
+    long_description="pre-commit-hooks",
+    long_description_content_type="text/markdown",
+    url="https://github.com/imjoseangel/pre-commit-hooks",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
